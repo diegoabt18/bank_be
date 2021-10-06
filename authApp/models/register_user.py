04a1acc45a7c5cedec033afa6a677068c7106aee
dabotello <diegoalbertbt@gmail.com>
2021-10-06 12:07:40 -0500

    registrar usuario 2

--- authApp/models/register_user.py
+++ authApp/models/register_user.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Register_user(models.Model):
     user_id = models.AutoField(primary_key=True)
-    name = models.CharField(ax_length = 50)
-    last_name = models.CharField(ax_length = 50)
+    name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
     nickname = models.CharField(max_length = 15, unique=True)
     password = models.CharField(max_length = 256)
     email = models.EmailField(max_length = 100)
