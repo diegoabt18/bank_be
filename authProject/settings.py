c7480f195d6926fddc61034c1ac3c0d018876c68
dabotello <diegoalbertbt@gmail.com>
2021-10-06 12:35:52 -0500

    registrar usuario 4

--- authProject/settings.py
+++ authProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,8 +154,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
-import django_heroku
 django_heroku.settings(locals())
-
