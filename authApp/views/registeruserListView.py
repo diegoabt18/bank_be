c0220fb450dfb66737267e5374c100f551106fa1
dabotello <diegoalbertbt@gmail.com>
2021-10-06 18:42:19 -0500

    registrar usuario 5

--- authApp/views/registeruserListView.py
+++ authApp/views/registeruserListView.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from rest_framework import generics, status
+from rest_framework import views, status
 from rest_framework.response import Response
 from authApp.models.register_user import Register_user
 from rest_framework.permissions import IsAuthenticated
 from authApp.serializers.registeruserSerializer import RegisteruserSerealizer
 
-class RegisteruserListView(generics.RetrieveAPIView):
+class RegisteruserListView(views.APIView):
     queryset = Register_user.objects.all()
     serializer_class = RegisteruserSerealizer
     permission_classes = (IsAuthenticated,)
