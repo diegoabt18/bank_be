d5cbb4fc8fe45d541437688a98a5b497ce30e597
dabotello <diegoalbertbt@gmail.com>
2021-10-06 19:00:37 -0500

    registrar usuario 6

--- authApp/views/__init__.py
+++ authApp/views/__init__.py
@@ -1,3 +1,4 @@
 from .userCreateView import UserCreateView
 from .userDetailView import UserDetailView
 from .registeruserCreateView import RegisteruserCreateview
+from .registeruserListView import RegisteruserListView
