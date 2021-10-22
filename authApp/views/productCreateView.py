d63e02c9b0cbb3bbec6407e244ee0b07506ccf8a
dabotello <diegoalbertbt@gmail.com>
2021-10-22 08:23:55 -0500

    despliegue

--- authApp/views/productCreateView.py
+++ authApp/views/productCreateView.py
@@ -22,8 +22,8 @@ class ProductCreateView(generics.CreateAPIView):
 
         serializer = ProductSerializer(data=request.data['product_data'])
         print(request.FILES)
-        datos = request.data['fileA']
-        print(datos)
+        # datos = request.data['fileA']
+        # print(datos)
    
         serializer.is_valid(raise_exception=True)
         serializer.save()
