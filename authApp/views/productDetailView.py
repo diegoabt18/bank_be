06066115717225746495a0e896b6c64960e13954
mjulissagonzalez@gmail.com <mjulissagonzalez@gmail.com>
2021-10-26 09:41:31 -0500

    Julissa Deployment 6

--- authApp/views/productDetailView.py
+++ authApp/views/productDetailView.py
@@ -7,14 +7,12 @@ from rest_framework.permissions import IsAuthenticated
 from authApp.models.product import Product
 from authApp.serializers.productSerializer import ProductSerializer
 from django.core import serializers
+from django.http import HttpResponse
 
 class ProductDetailView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
     permission_classes = (IsAuthenticated, )
-    queryset = Product.objects.all()
-    print("***********************************")
-    allProducts = serializers.serialize("json", queryset)
-    print(allProducts)
+
 
     def get(self, request, *args, **kwargs):
         token = request.META.get('HTTP_AUTHORIZATION')[7:]
@@ -24,6 +22,12 @@ class ProductDetailView(generics.RetrieveAPIView):
         if valid_data['user_id'] != kwargs['user']:
             stringResponse = {'detail':'Acceso no autorizado - Información detallada de Producto'}
             return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+        
+        queryset = Product.objects.all()
+        print("***********************************")
+        allProducts = serializers.serialize("json", queryset)
+        print(allProducts)
 
-        return super().get(request, *args, **kwargs)    
+        # return super().get(request, *args, **kwargs)    
+        return HttpResponse(allProducts, content_type='application/json')
 
